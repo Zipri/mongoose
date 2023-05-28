e236f6ddfdf0b5e792b323857c4e9e4bcd2b5c95
Seroshtan_KV <seroshtan_kv@rtsoft.ru>
2023-05-28 22:11:15 +0300

    Analyzes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import {Routes, Route} from "react-router-dom";
-import Add from "./components/Add";
 import Find from "./components/find";
 import StartPage from "./components/StartPage";
 import PatientRegistration from "./components/PatientRegistration";
 import ManagingMedicalRecords from "./components/ManagingMedicalRecords";
+import Analyzes from "./components/Analyzes";
 
 
 const App = () => {
@@ -16,7 +16,7 @@ const App = () => {
       <Route path={'/'} element={<StartPage/>}/>
       <Route path={'/patient_registration'} element={<PatientRegistration/>}/>
       <Route path={'/managing_medical_records'} element={<ManagingMedicalRecords/>}/>
-      <Route path={'/analyzes'} element={<Find/>}/>
+      <Route path={'/analyzes'} element={<Analyzes/>}/>
       <Route path={'/images'} element={<Find/>}/>
       <Route path={'/authorization'} element={<Find/>}/>
     </Routes>
